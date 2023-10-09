f2f36afaef873bdf7bc7242c0a3d547272ebbdc5
Ruslan <ruslanchikbest@gmail.com>
2023-10-09 19:13:48 +0300

    Typing the constAuto

---
+++ src/components/auto/constAuto.tsx
@@ -0,0 +1,12 @@
+
+
+// sorted
+
+export const brand:string = 'Brand';
+export const year:string = 'Year';
+export const price :string= 'Price';
+export const raitingMin:string = 'Raiting--';
+export const raitingPlus:string = 'Raiting++';
+export const sortedBy:string = 'Sorted by';
+export const found:string = 'Cars not found :(';
+
