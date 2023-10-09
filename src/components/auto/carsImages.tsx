d50536c807f36671cc13b841113015ad1e2c7f27
Ruslan <ruslanchikbest@gmail.com>
2023-10-09 19:17:53 +0300

    Typing the carsImages

---
+++ src/components/auto/carsImages.tsx
@@ -0,0 +1,15 @@
+
+import audi from '../img/audi.jpg'
+import bmw from '../img/bmw.jpg'
+import merc from '../img/merc.jpg'
+import lada from '../img/lada.jpg'
+import toyota from '../img/toyota.jpg'
+import mitsub from '../img/mits.jpg'
+
+
+export const audiCar:string = audi;
+export const bmwCar :string= bmw;
+export const mercCar:string = merc;
+export const ladaCar:string = lada;
+export const toyotaCar:string = toyota;
+export const mitsubCar:string = mitsub;
