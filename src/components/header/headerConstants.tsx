cada47a6a51a8fae779603f64dca7742e2bfba57
Ruslan <ruslanchikbest@gmail.com>
2023-10-09 15:17:19 +0300

    Typing the headerConstans

---
+++ src/components/header/headerConstants.tsx
@@ -0,0 +1,23 @@
+import enterImage from '../img/header/enter.png'
+import okImage from '../img/header/ok.png'
+
+
+
+
+export const enterProfileTitle:string = 'Enter in your profile'
+export const emailMessageError:string = 'Enter your e-mail'
+export const passwordMessageError :string = 'Enter your password'
+export const userNumberMessageError:string = 'Enter your phone number (only numbers not less than 10)'
+export const incorrectPass:string = 'Password must be more than 5 characters'
+export const passMismatch :string= 'Password mismatch'
+export const incorrectEmail:string = 'Incorrect e-mail'
+export const showPassLabel:string = 'Show password'
+export const enter:string = 'Enter'
+export const reg:string = 'Registration'
+
+export const userNameErrorMesssage:string = 'Enter your name'
+
+export const enterSite:string = enterImage;
+
+
+export const okImg:string = okImage
