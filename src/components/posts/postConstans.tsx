bcae18ccd614d074eccd600cc7c27518ad85eb87
Ruslan <ruslanchikbest@gmail.com>
2023-10-09 19:10:58 +0300

    Typing the postConstants

---
+++ src/components/posts/postConstans.tsx
@@ -0,0 +1,17 @@
+import edit from './redact.png';
+import del from './delete.png';
+
+// form
+export const messageTitleError:string = 'Enter name';
+export const messageBodyError:string = 'Enter discription';
+export const messageFieldError:string = 'Fill in all fields of the form';
+export const editPostImage:string = edit;
+export const deletePostImage:string = del;
+
+export const addComm:string = 'Add Comment';
+export const formPostTitle:string = 'Create Post';
+
+// postlist
+export const addPost:string = 'Add post';
+export const notFound:string = 'Comments not found';
+export const sortedBy:string = 'Sorted By:';
