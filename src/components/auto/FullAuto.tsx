cae85efd2adfbd1ece949e0c34e91ad4a4428b91
Ruslan <ruslanchikbest@gmail.com>
2023-10-11 17:11:44 +0300

    Create button back

--- src/components/auto/FullAuto.tsx
+++ src/components/auto/FullAuto.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
+import { useParams,useNavigate } from 'react-router'
 import axios from 'axios'
 import { AutoType } from './Auto'
 import Loader from '../loader/Loader'
+
 
 const FullAuto:React.FC = () => {
   const [isLoad, setIsload] = useState<boolean>(false)
   const [fullAuto, setFullAuto] = useState<AutoType>()
   const [errorFullAutoLoading, setErrorFullAutoLoading] = useState<string>('')
     const {id} = useParams()
+    const navigate = useNavigate()
 
     useEffect(() => {
       async function FetchAuto(){
@@ -41,6 +43,7 @@ return  <Loader/>
 <h2>{fullAuto.year}</h2>
 <h2>{fullAuto.price}</h2>
 <img src={fullAuto.imageUrl} alt="" />
+ <button onClick={() => navigate('/')}>Back</button>
      
     </div>
   )
