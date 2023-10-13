42893a0d597c1d750001405aee2a36f6162638d3
Ruslan <ruslanchikbest@gmail.com>
2023-10-13 18:07:59 +0300

    create slider

--- src/components/auto/fullauto/Collection.tsx
+++ src/components/auto/fullauto/Collection.tsx
@@ -14,10 +14,27 @@ const Collection: React.FC<CollectionProps> = ({ images, imageStart }) => {
 
    const [activeImage, setActiveImage] = React.useState<string>(imageStart)
    const [modal, setModal] = React.useState(false)
+   const [current, setCurrent] = React.useState(1)
+
+   const length = images.length;
+
 
    const showBigPhoto = (image: imagesType, i: number) => {
       setActiveImage(image.imgSrc)
-      console.log(typeof image.imgSrc);
+   }
+
+
+   const prevSlide = () => {
+      setCurrent(current === 0 ? length - 1 : current - 1)
+      console.log(length);
+   }
+   const nextSlide = () => {
+      setCurrent(current === length - 1 ? 0 : current + 1)
+      console.log(length);
+
+   }
+   if (!Array.isArray(images) || images.length <= 0) {
+      return null
    }
 
 
@@ -26,9 +43,29 @@ const Collection: React.FC<CollectionProps> = ({ images, imageStart }) => {
       <div>
          <div className={cl.photo}>
             <img onClick={() => setModal(true)} className={cl.image} src={activeImage} alt="auto" />
+            {/* <MyModal visible={modal} changeVisible={setModal}>
+               <div className={cl.imageModalWrapper}>
+                  <img className={cl.imageModal} src={activeImage} alt="auto" />
+               </div>
+             
+            </MyModal> */}
+
             <MyModal visible={modal} changeVisible={setModal}>
-               <div className={cl.imageModalWrapper}>  <img className={cl.imageModal} src={activeImage} alt="auto" /></div>
+
+
+               {images.map((image: imagesType, i: number) => (<div className={cl.imageModalWrapper} key={i} >
+                  {i === current && (
+                     <div >
+                        <button className={cl.btnPrev} onClick={prevSlide}>Prev</button>
+                        <img className={cl.imageModal} src={image.imgSrc} />
+                        <button className={cl.btnNext} onClick={nextSlide}>Next</button>
+                     </div>)}
+               </div>))}
+
+
+
             </MyModal>
+
          </div>
 
          <div className={cl.smallImagesWrapper}>
