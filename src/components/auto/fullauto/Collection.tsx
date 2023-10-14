c21560ae34eb57d10d208be6787cd7db535153a4
Ruslan <ruslanchikbest@gmail.com>
2023-10-14 13:31:45 +0300

    Fixing errors

--- src/components/auto/fullauto/Collection.tsx
+++ src/components/auto/fullauto/Collection.tsx
@@ -9,29 +9,31 @@ import next from '../../img/next.png';
 interface CollectionProps {
    images: imagesType[];
    imageStart: string;
+
 }
 
 const Collection: React.FC<CollectionProps> = ({ images, imageStart }) => {
 
+
+
    const [activeImage, setActiveImage] = React.useState<string>(imageStart)
    const [modal, setModal] = React.useState(false)
-   const [current, setCurrent] = React.useState(1)
+   const [current, setCurrent] = React.useState(0)
 
    const length = images.length;
 
 
-   const showBigPhoto = (image: imagesType, i: number) => {
+   const showBigPhoto = (image: imagesType) => {
       setActiveImage(image.imgSrc)
    }
 
 
    const prevSlide = () => {
       setCurrent(current === 0 ? length - 1 : current - 1)
-      console.log(length);
+
    }
    const nextSlide = () => {
       setCurrent(current === length - 1 ? 0 : current + 1)
-      console.log(length);
 
    }
    if (!Array.isArray(images) || images.length <= 0) {
@@ -44,34 +46,43 @@ const Collection: React.FC<CollectionProps> = ({ images, imageStart }) => {
       <div>
          <div className={cl.photo}>
             <img onClick={() => setModal(true)} className={cl.image} src={activeImage} alt="auto" />
-            {/* <MyModal visible={modal} changeVisible={setModal}>
-               <div className={cl.imageModalWrapper}>
-                  <img className={cl.imageModal} src={activeImage} alt="auto" />
-               </div>
-             
-            </MyModal> */}
+
 
             <MyModal visible={modal} changeVisible={setModal}>
 
-
-               {images.map((image: imagesType, i: number) => (<div className={cl.imageModalWrapper} key={i} >
-                  {i === current && (
-                     <div >
-                        <img src={next} className={cl.btnPrev} onClick={nextSlide} />
-                        <img className={cl.imageModal} src={image.imgSrc} />
-                        <img src={next} className={cl.btnNext} onClick={nextSlide} />
-                     </div>)}
-               </div>))}
-
-
+               {images.map((image: imagesType, i: number) => (
+                  // <div className={cl.imageModalWrapper} key={i} >
+                  <div className={cl.imageModalWrapper} key={i} >
+                     {i === current && (
+                        <>
+                           <img src={next} className={cl.btnPrev} onClick={prevSlide} />
+                           <img className={cl.imageModal} src={image.imgSrc} />
+                           <img src={next} className={cl.btnNext} onClick={nextSlide} />
+                        </>)}
+                  </div>))}
 
             </MyModal>
+            {/* <MyModal visible={modal} changeVisible={setModal}>
+               {images.map((slide, index) => {
+                  return (
+                     <div
+                        className={index === current ? 'slide active' : 'slide'}
+                        key={index}
+                     >
+                        {index === current && (
+                           <img src={slide.imgSrc} alt='travel image' />
+                        )}
+                     </div>
+                  );
+               })}
+
+            </MyModal> */}
 
          </div>
 
          <div className={cl.smallImagesWrapper}>
             {images.map((image: imagesType, i: number) => (<div key={i} className={cl.smallImages} >
-               <img onClick={() => showBigPhoto(image, i)} className={cl.imageSmall} src={image.imgSrc} />
+               <img onClick={() => showBigPhoto(image)} className={cl.imageSmall} src={image.imgSrc} />
             </div>))}
          </div>
 
